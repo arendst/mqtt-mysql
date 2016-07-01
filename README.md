36bd99fb0fe2de80620030430838d7a07dedc813
arendst <mhtarends@gmail.com>
2016-07-01 17:21:23 +0200

    Simplified database and add admin tool

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mqtt-mysql
 MQTT mysql client and server
+
+Run ```mqtt-mysql-admin``` to clean up database.
--- messages.sql
+++ messages.sql
@@ -1,25 +1,13 @@
 CREATE DATABASE mqtt;
 USE mqtt;
 
-DROP TABLE IF EXISTS topic;
-CREATE TABLE topic (
-  top_id     int(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  topic      text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS message;
-CREATE TABLE message (
-  mes_id      int(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+  timestamp   timestamp DEFAULT CURRENT_TIMESTAMP,
+  topic       text NOT NULL,
+  qos         tinyint(1) NOT NULL,
   message     text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS messages;
-CREATE TABLE messages2 (
-  timestamp   timestamp DEFAULT CURRENT_TIMESTAMP,
-  topic_id    int(4) NOT NULL,
-  qos         tinyint(1) NOT NULL,
-  message_id  int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 DROP TABLE IF EXISTS settings;
 CREATE TABLE settings (
